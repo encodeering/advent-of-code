323222e8b8a1f0f1a21508fa2ee183e6c26b0aed
Michael Clausen <encodeering@gmail.com>
2016-12-03 09:12:38 +0100

    initializes advent of code repository
    * kotlin archetype

---
+++ src/main/kotlin/com/encodeering/aoc/y2016/Application.kt
@@ -0,0 +1,13 @@
+package com.encodeering.aoc.y2016
+
+/**
+ * @author Michael Clausen - encodeering@gmail.com
+ */
+object Application {
+
+    @JvmStatic
+    fun main (args : Array<String>) {
+        println ("running ${args.joinToString ()}")
+    }
+
+}
