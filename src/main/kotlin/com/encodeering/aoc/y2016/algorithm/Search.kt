1d6cbe0f0d158745d3f7b89712ca56015fc042c5
Michael Clausen <encodeering@gmail.com>
2016-12-17 13:38:13 +0100

    adds a tree based algorithm for optimization problems
    * storage can be used as strategy for bfs, dfs, a-star
    * identity function can be used for morph, if its not relevant, which leads to T = M
    * generate refers the problem generator function
    
    NOTE
    
    * started implementation with day 11, 13

---
+++ src/main/kotlin/com/encodeering/aoc/y2016/algorithm/Search.kt
@@ -0,0 +1,40 @@
+package com.encodeering.aoc.y2016.algorithm
+
+/**
+ * @author clausen - encodeering@gmail.com
+ */
+class Search<T, M, R> (
+        private val storage  : () -> MutableCollection<Pair<T, R>>,
+        private val generate : (T, R) ->      Iterable<Pair<T, R>>,
+        private val morph    : (T)    -> M) {
+
+    fun query (start : T, neutral : R, steps : Int = 0, solves : Pair<T, R>.() -> Boolean) : R? {
+        val operations = storage ()
+            operations += start to neutral
+
+        val travels = mutableListOf<M> ()
+
+        var count = 0
+        var step  = 0
+
+        do {
+            if (             steps > 0) {
+                if (++step > steps) break
+            }
+
+            if (++count % 10000 == 0) println ("candidates [${operations.size}] buckets [${travels.size}]")
+
+            val                op = operations.first ()
+            operations.remove (op)
+
+            travels += morph (op.first)
+
+            if (op.solves ()) return op.second
+            else
+                 operations += generate (op.first, op.second).filterNot { (candidate, _) -> morph (candidate) in travels }
+        } while (operations.isNotEmpty ())
+
+        return operations.firstOrNull ()?.second
+    }
+
+}
