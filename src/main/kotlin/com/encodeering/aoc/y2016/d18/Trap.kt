243a44126651f942e52092d83cbb0ecf868d0e66
Michael Clausen <encodeering@gmail.com>
2016-12-17 17:18:22 +0100

    solves first part of day 18
    * http://adventofcode.com/2016/day/18

---
+++ src/main/kotlin/com/encodeering/aoc/y2016/d18/Trap.kt
@@ -0,0 +1,40 @@
+package com.encodeering.aoc.y2016.d18
+
+import com.encodeering.aoc.y2016.extension.window
+
+/**
+ * @author clausen - encodeering@gmail.com
+ */
+object Day18 {
+
+    @JvmStatic
+    fun main(args : Array<String>) {
+        println ("safe tiles: ${trapplan (".^.^..^......^^^^^...^^^...^...^....^^.^...^.^^^^....^...^^.^^^...^^^^.^^.^.^^..^.^^^..^^^^^^.^^^..^", 40).count { it == '.' }}")
+    }
+
+}
+
+fun trapplan            (row : CharSequence, length : Int) : CharSequence {
+    tailrec fun analyse (row : CharSequence, length : Int, plan : List<CharSequence>) : List<CharSequence> {
+        if (length <= 1) return plan
+
+        val next = ".$row.".toList ().window (3).map { w -> w.map (::trap) }.map {
+                  (left,     center,     right) ->
+                (  left &&   center && ! right) ||
+                (! left &&   center &&   right) ||
+                (  left && ! center && ! right) ||
+                (! left && ! center &&   right)
+        }.map {
+            when (it) {
+                true  -> '^'
+                false -> '.'
+            }
+        }.joinToString ("")
+
+        return analyse (next, length - 1, plan + next)
+    }
+
+    return analyse (row, length, listOf (row)).joinToString ("\n")
+}
+
+fun trap (char : Char) = char == '^'
