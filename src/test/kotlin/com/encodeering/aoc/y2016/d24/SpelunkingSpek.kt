5f1afb1a4180e06241ca39cc04a5211d5c28cd79
Michael Clausen <encodeering@gmail.com>
2017-11-14 12:28:32 +0100

    solves first part of day 24
    * http://adventofcode.com/2016/day/24

---
+++ src/test/kotlin/com/encodeering/aoc/y2016/d24/SpelunkingSpek.kt
@@ -0,0 +1,27 @@
+package com.encodeering.aoc.y2016.d24
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+
+/**
+ * @author clausen - encodeering@gmail.com
+ */
+@RunWith(JUnitPlatform::class)
+class SpelunkingSpek : Spek({
+
+    describe ("Spelunking") {
+
+        describe ("#1") {
+
+            it ("first example") {
+
+            }
+
+        }
+
+    }
+
+})
