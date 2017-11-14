d5b116cc39e7867e64cb06fc0de64e32425f9b5e
Michael Clausen <encodeering@gmail.com>
2017-11-14 11:50:44 +0100

    adds a utility to calculate the permutations of a collection
    * a permutations is wrapped by a list to keep the order

--- src/main/kotlin/com/encodeering/aoc/y2016/algorithm/Statistic.kt
+++ src/main/kotlin/com/encodeering/aoc/y2016/algorithm/Statistic.kt
@@ -15,3 +15,24 @@ fun <T, S> Iterable<T>.cartesian (other : Iterable<S>) : Iterable<Pair<T, S>> =
                              a ->
             other.map { b -> a to b }
         }.distinct ()
+
+
+fun <T> Iterator<T>.nextOrNull () = if (hasNext ()) next () else null
+
+fun <T> Collection<T>.permutation () : Sequence<List<T>> {
+    if (size == 1) return sequenceOf (toList ())
+
+    val outer = iterator ()
+
+    var head : T? = null
+    var inners = emptyList<Collection<T>> ().iterator ()
+
+    fun next () : Collection<T>? = inners.nextOrNull () ?: outer.nextOrNull ()?.run {
+        head   =                     this
+        inners = (this@permutation - this).permutation ().iterator ()
+
+        next ()
+    }
+
+    return generateSequence { next ()?.run { listOf (head!!) + this } }
+}
--- src/test/kotlin/com/encodeering/aoc/y2016/algorithm/StatisticSpek.kt
+++ src/test/kotlin/com/encodeering/aoc/y2016/algorithm/StatisticSpek.kt
@@ -37,6 +37,17 @@ class StatisticSpek : Spek({
 
         }
 
+
+        it ("permutation") {
+            expect (listOf (1, 2, 3).permutation ().toList ()).to.equal (listOf (
+                listOf (1, 2, 3),
+                listOf (1, 3, 2),
+                listOf (2, 1, 3),
+                listOf (2, 3, 1),
+                listOf (3, 1, 2),
+                listOf (3, 2, 1)
+            ))
+        }
     }
 
 })
