5486b0a7a05145c674f10f90782f41125ee9ab55
Michael Clausen <encodeering@gmail.com>
2017-11-16 09:21:25 +0100

    defines a companion object for common search algorithms generators
    * bfs, dfs, and a*
    
    NOTE
    
    * morph can't be made optional, as we can't say that M equals T to the same time, which is not the common case

--- src/main/kotlin/com/encodeering/aoc/common/Search.kt
+++ src/main/kotlin/com/encodeering/aoc/common/Search.kt
@@ -1,5 +1,9 @@
 package com.encodeering.aoc.common
 
+import java.util.LinkedList
+import java.util.PriorityQueue
+import java.util.Stack
+
 /**
  * @author clausen - encodeering@gmail.com
  */
@@ -8,6 +12,34 @@ class Search<T, M, R> (
     private val generate : (T, R) ->      Iterable<Pair<T, R>>,
     private val morph    : (T)    -> M
 ) {
+
+    companion object {
+
+        fun <T,    R> bfs (generate : (T, R) -> Iterable<Pair<T, R>>) = bfs (generate) { it }
+
+        fun <T, M, R> bfs (generate : (T, R) -> Iterable<Pair<T, R>>, morph : (T) -> M) = Search (
+            storage  = { LinkedList () },
+            morph    = morph,
+            generate = generate
+        )
+
+        fun <T,    R> dfs (generate : (T, R) -> Iterable<Pair<T, R>>) = dfs (generate) { it }
+
+        fun <T, M, R> dfs (generate : (T, R) -> Iterable<Pair<T, R>>, morph : (T) -> M) = Search (
+            storage  = { Stack () },
+            morph    = morph,
+            generate = generate
+        )
+
+        fun <T,    R> astar (comparator : Comparator<Pair<T, R>>, generate : (T, R) -> Iterable<Pair<T, R>>) = astar (comparator, generate) { it }
+
+        fun <T, M, R> astar (comparator : Comparator<Pair<T, R>>, generate : (T, R) -> Iterable<Pair<T, R>>, morph : (T) -> M) = Search (
+            storage  = { PriorityQueue (comparator) },
+            morph    = morph,
+            generate = generate
+        )
+
+    }
 
     fun query (start : T, neutral : R, solves : Pair<T, R>.() -> Boolean) : R? {
         val operations = storage ()
