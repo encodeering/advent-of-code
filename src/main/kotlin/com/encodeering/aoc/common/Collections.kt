0237381e84c0f9ead87c7bcec0a82a3bc73d5256
Michael Clausen <encodeering@gmail.com>
2017-11-20 15:17:10 +0100

    removes any from type parameter of collectables
    * any requires type definitions to use the same boundary, so its more convenient if we can skip that
    
    NOTE
    
    * breaking change: transformation function now works on the element instead

--- src/main/kotlin/com/encodeering/aoc/common/Collections.kt
+++ src/main/kotlin/com/encodeering/aoc/common/Collections.kt
@@ -5,19 +5,19 @@ package com.encodeering.aoc.common
  */
 fun <T> Iterator<T>.nextOrNull () = if (hasNext ()) next () else null
 
-fun <T : Any>    Sequence<T>.window (n : Int, step : Int = 1, partial : Boolean = false) = window (n, step, partial) { it }
-fun <T : Any, R> Sequence<T>.window (n : Int, step : Int = 1, partial : Boolean = false, transform : (List<T>) -> R) : Sequence<R> {
+fun <T>    Sequence<T>.window (n : Int, step : Int = 1, partial : Boolean = false) = window (n, step, partial) { it }
+fun <T, R> Sequence<T>.window (n : Int, step : Int = 1, partial : Boolean = false, transform : (T) -> R) : Sequence<List<R>> {
     val source  = iterator ()
     val windows = mutableMapOf<Int, List<T>> ().withDefault { emptyList () }
 
     fun xy (number : Int) = number % n to number / n
 
-    return generateSequence (0) { it + step }.mapNotNull {
+    return generateSequence (0) { it + step }.map {
          start ->
-        (start until start + n).map (::xy).mapNotNull { (x, y) ->
+        (start until start + n).map (::xy).map { (x, y) ->
             windows.computeIfAbsent (y) { source.asSequence ().take (n).toList () }
             windows.getValue (y).getOrNull (x)
-        }.also {
+        }.filter { it != null }.also {
             val (x,                 y) = xy (start)
             if  (x == 0) (maxOf (0, y - 1) until y).forEach { windows.remove (it) }
         }
@@ -26,20 +26,22 @@ fun <T : Any, R> Sequence<T>.window (n : Int, step : Int = 1, partial : Boolean 
             partial -> it.isNotEmpty ()
             else    -> it.size == n
         }.also { if (! it) windows.clear () }
-    }.map (transform)
+    }.map {
+        it.filter { it != null }.map { transform (it!!) }
+    }
 }
 
-fun <T : Any>    Sequence<T>.zipwise () = zipwise { it }
-fun <T : Any, R> Sequence<T>.zipwise (transform : (List<T>) -> R) = window (2, 1, transform = transform)
+fun <T>    Sequence<T>.zipwise () = zipwise { it }
+fun <T, R> Sequence<T>.zipwise (transform : (T) -> R) = window (2, 1, transform = transform)
 
-fun <T : Any>    Sequence<T>.blockwise (n: Int, partial : Boolean = false) = blockwise (n, partial) { it }
-fun <T : Any, R> Sequence<T>.blockwise (n: Int, partial : Boolean = false, transform : (List<T>) -> R) = window (n, n, partial, transform)
+fun <T>    Sequence<T>.blockwise (n: Int, partial : Boolean = false) = blockwise (n, partial) { it }
+fun <T, R> Sequence<T>.blockwise (n: Int, partial : Boolean = false, transform : (T) -> R) = window (n, n, partial, transform)
 
-fun <T : Any>    Iterable<T>.window (n : Int, step : Int = 1, partial : Boolean = false) = window (n, step, partial) { it }
-fun <T : Any, R> Iterable<T>.window (n : Int, step : Int = 1, partial : Boolean = false, transform : (List<T>) -> R) = asSequence ().window (n, step, partial, transform).asIterable ()
+fun <T>    Iterable<T>.window (n : Int, step : Int = 1, partial : Boolean = false) = window (n, step, partial) { it }
+fun <T, R> Iterable<T>.window (n : Int, step : Int = 1, partial : Boolean = false, transform : (T) -> R) = asSequence ().window (n, step, partial, transform).asIterable ()
 
-fun <T : Any>    Iterable<T>.zipwise () = zipwise { it }
-fun <T : Any, R> Iterable<T>.zipwise (transform : (List<T>) -> R) = asSequence ().zipwise (transform).asIterable ()
+fun <T>    Iterable<T>.zipwise () = zipwise { it }
+fun <T, R> Iterable<T>.zipwise (transform : (T) -> R) = asSequence ().zipwise (transform).asIterable ()
 
-fun <T : Any>    Iterable<T>.blockwise (n : Int, partial : Boolean = false) = blockwise (n, partial) { it }
-fun <T : Any, R> Iterable<T>.blockwise (n : Int, partial : Boolean = false, transform : (List<T>) -> R) = asSequence ().blockwise (n, partial, transform).asIterable ()
+fun <T>    Iterable<T>.blockwise (n : Int, partial : Boolean = false) = blockwise (n, partial) { it }
+fun <T, R> Iterable<T>.blockwise (n : Int, partial : Boolean = false, transform : (T) -> R) = asSequence ().blockwise (n, partial, transform).asIterable ()
