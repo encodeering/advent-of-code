36e520ad19768a62be3fea3c4b299b97d51dd64a
Michael Clausen <encodeering@gmail.com>
2017-12-09 15:47:55 +0100

    solves first part of day 9
    * count escape characters, uneven means that the following character is escaped
    * tracklistener, compare with sax

---
+++ src/test/kotlin/com/encodeering/aoc/y2017/d9/GarbageSpek.kt
@@ -0,0 +1,35 @@
+package com.encodeering.aoc.y2017.d9
+
+import com.winterbe.expekt.expect
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+
+/**
+ * @author clausen - encodeering@gmail.com
+ */
+@RunWith(JUnitPlatform::class)
+class GarbageSpek : Spek ({
+
+    describe ("Garbage") {
+
+        describe ("#1") {
+
+            it ("first example") {
+                expect (count1 ("{}")).to.equal (1)
+                expect (count1 ("{{{}}}")).to.equal (6)
+                expect (count1 ("{{},{}}")).to.equal (5)
+                expect (count1 ("{{{},{},{{}}}}")).to.equal (16)
+                expect (count1 ("{<a>,<a>,<a>,<a>}")).to.equal (1)
+                expect (count1 ("{{<ab>},{<ab>},{<ab>},{<ab>}}")).to.equal (9)
+                expect (count1 ("{{<!!>},{<!!>},{<!!>},{<!!>}}")).to.equal (9)
+                expect (count1 ("{{<a!>},{<a!>},{<a!>},{<ab>}}")).to.equal (3)
+            }
+
+        }
+
+    }
+
+})
