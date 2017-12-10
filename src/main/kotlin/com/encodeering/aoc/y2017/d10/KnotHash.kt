245841f957474c6211dd38196b3bf64e39cbabe0
Michael Clausen <encodeering@gmail.com>
2017-12-10 09:50:37 +0100

    solves second part of day 10
    * create chunks (blocks) with size equal to 16 and reduce them with xor, then pad them to if hex string is not of size 2

--- src/main/kotlin/com/encodeering/aoc/y2017/d10/KnotHash.kt
+++ src/main/kotlin/com/encodeering/aoc/y2017/d10/KnotHash.kt
@@ -1,7 +1,9 @@
 package com.encodeering.aoc.y2017.d10
 
+import com.encodeering.aoc.common.blockwise
 import com.encodeering.aoc.common.rotate
 import com.encodeering.aoc.common.traverse
+import java.lang.Integer.toHexString
 
 /**
  * @author clausen - encodeering@gmail.com
@@ -10,13 +12,21 @@ fun main(args : Array<String>) {
     traverse ("/y2017/d10/knots.txt") {
         println ("mulitplication:  ${hash1 ((0..255).toList(), it.first().split(',').map { it.toInt () })}")
     }
+
+    traverse ("/y2017/d10/knots.txt") {
+        println ("hash:  ${hash2 ((0..255).toList(), it.first())}")
+    }
 }
 
 fun hash1 (list : List<Int>, knots : List<Int>) = list.hash (knots).run { this[0] * this[1] }
 
-private fun List<Int>.hash                 (knots : List<Int>) : List<Int> {
-    tailrec fun combine (input : List<Int>, knots : List<Int>, position : Int, skip : Int) : List<Int> {
-        if                                 (knots.isEmpty ()) return input
+fun hash2 (list : List<Int>, knots : CharSequence) = list.hash (knots.ascii () + listOf (17, 31, 73, 47, 23), 64)
+    .blockwise   (16)
+        .joinToString ("") { toHexString (it.reduce { a, b -> a xor b }).padStart (2, '0') }
+
+private fun List<Int>.hash                 (knots : List<Int>, runs : Int = 1) : List<Int> {
+    tailrec fun combine (input : List<Int>, knots : List<Int>, position : Int, skip : Int) : Knot {
+        if                                 (knots.isEmpty ()) return Knot (input, knots, position, skip)
 
         val list =            input + input
         val from = position % input.size
@@ -29,10 +39,17 @@ private fun List<Int>.hash                 (knots : List<Int>) : List<Int> {
         )
     }
 
-    return combine (this, knots, 0, 0)
+    return (0 until runs).fold (Knot (this, knots, 0, 0)) {
+                 knot, _ ->
+        combine (knot.list, knot.knots, knot.position, knot.skip).copy (knots = knot.knots)
+    }.list
 }
 
 fun <T> List<T>.reverse (idxA : Int = 0, idxB : Int = size - 1) : List<T> =
     subList (0,    idxA) +
     subList (idxA, idxB + 1).reversed () +
     subList (idxB + 1, size)
+
+private data class Knot (val list : List<Int>, val knots : List<Int>, val position : Int, val skip : Int)
+
+private fun CharSequence.ascii () = map { c -> c.toInt () }.toList()
