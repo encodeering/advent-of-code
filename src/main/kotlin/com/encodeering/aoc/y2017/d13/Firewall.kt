182764af4aaf13e3ab9cd184a5077356991bf709
Michael Clausen <encodeering@gmail.com>
2017-12-13 18:15:12 +0100

    seperates layer generation from severity checks
    * a full cycle is, as said before, reached in 2 * depth - 2 cycles, which means that its dangerous if it contains one layer with zero as position
    
    NOTE
    
    * reduces costs of layer constructions as we are interested in the first safe layer only

--- src/main/kotlin/com/encodeering/aoc/y2017/d13/Firewall.kt
+++ src/main/kotlin/com/encodeering/aoc/y2017/d13/Firewall.kt
@@ -16,36 +16,40 @@ fun main(args : Array<String>) {
     }
 }
 
-fun severity1   (lines : Sequence<String>) = severity (lines).first ().filter { it.position == 0 }.sumBy { it.number * it.depth }
+fun severity1   (lines : Sequence<String>) = severity (lines).first ().let { (delay, firewall) -> firewall.severity  (delay) }
 
-fun severity2   (lines : Sequence<String>) = severity (lines).takeWhile { it.any { it.position == 0 } }.count ()
+fun severity2   (lines : Sequence<String>) = severity (lines).takeWhile {    (delay, firewall) -> firewall.dangerous (delay) }.count ()
 
-fun severity    (lines : Sequence<String>) : Sequence<List<Layer>> {
-    val layers = lines.map {
+fun severity    (lines : Sequence<String>) : Sequence<Pair<Int, Firewall>> {
+    val firewall = Firewall (lines.map {
         it.findAll ("(\\d+)".toRegex ()).toList ().let {
                    (layer,          depth) ->
              Layer (layer.toInt (), depth.toInt (), 0)
         }
-    }.toList ()
+    }.toList ())
 
+    return generateSequence (0) { it + 1 }.map { it to firewall }
+}
 
-    // could be optimized: layers of depth 2 will have a severity every second time
-    return generateSequence (0) { it + 1 }.map { delay ->
-        layers.map {
-            layer ->
-            layer.copy (position = layer.run {
-                val steps   = depth - 1
+data class Firewall (private val  layers : List<Layer>) {
 
-                val value   = number + delay
+    fun dangerous (delay : Int) = layers.any { (it.number + delay) % (2 * it.depth - 2) == 0 }
 
-                val partial = value % steps
-                val full    = value / steps
+    fun severity  (delay : Int) = layers.map {
+        layer ->
+        layer.copy (position = layer.run {
+            val steps   = depth - 1
 
-                if (full % 2 == 0)         partial
-                else               depth - partial - 1
-            })
-        }
-    }
+            val value   = number + delay
+
+            val partial = value % steps
+            val full    = value / steps
+
+            if (full % 2 == 0)         partial
+            else               depth - partial - 1
+        })
+    }.filter { it.position == 0 }.sumBy { it.number * it.depth }
+
 }
 
 data class Layer (val number : Int, val depth : Int, val position : Int)
