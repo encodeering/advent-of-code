730af1918c483b27f42830a688dbe7db2450853e
Michael Clausen <encodeering@gmail.com>
2017-12-17 04:52:55 +0100

    solves second part of day 16
    * searching for a repitition counter, as 1 billion times is quite a huge amount - even for a very fast algorithm
    ** remainder operation can be then used to eliminate full cycles

--- src/main/kotlin/com/encodeering/aoc/y2017/d16/Promenade.kt
+++ src/main/kotlin/com/encodeering/aoc/y2017/d16/Promenade.kt
@@ -12,10 +12,37 @@ fun main (args : Array<String>) {
         val moves = it.first ().split(',')
 
         println ("dance #1: ${dance1 (moves)}")
+        println ("dance #2: ${dance2 (moves)}")
     }
 }
 
 private fun dance1 (moves : List<String>) = "abcdefghijklmnop".promenade (moves)
+
+private fun dance2 (moves : List<String>) : CharSequence {
+    val text : CharSequence = "abcdefghijklmnop"
+    val cycles = 1000000000 % repetition (text, moves)
+
+    return (0 until cycles).fold (text) { t, _ -> t.promenade (moves) }
+}
+
+private fun repetition (text : CharSequence, moves : List<String>) : Int {
+    var sample = text
+    var count  = 0
+
+    val map = mutableMapOf<CharSequence, Int>()
+        map[sample] = 1
+
+    while (true) {
+            sample  =                   sample.promenade (moves)
+        map[sample] = map.getOrDefault (sample, 0) + 1
+
+        count += 1
+
+        if (map[sample]!! > 1) {
+            return count
+        }
+    }
+}
 
 fun CharSequence.promenade (moves : List<String>): CharSequence {
     return moves.fold (this) { p, move ->
