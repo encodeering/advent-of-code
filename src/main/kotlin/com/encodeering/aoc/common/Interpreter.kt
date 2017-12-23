5264b4fc00dac9e8def66e56caa34d13095dc107
Michael Clausen <encodeering@gmail.com>
2017-12-23 15:19:29 +0100

    extracts interpreter code to common package
    * state, code and interpreter can be reused by different language puzzles

---
+++ src/main/kotlin/com/encodeering/aoc/common/Interpreter.kt
@@ -0,0 +1,80 @@
+package com.encodeering.aoc.common
+
+/**
+ * @author clausen - encodeering@gmail.com
+ */
+class Code (private val instructions : MutableList<Command>, private val optimizer : (MutableList<Command>) -> MutableList<Command> = { it }) {
+
+    var line : Int = 0
+
+    var world : MutableList<Command> = instructions
+
+    val size : Int get () = world.size
+
+    operator fun get (line : Int) : Command? =
+        world.getOrElse (line) { null }
+
+    operator fun set (line : Int, value : Command) {
+        deoptimize ()
+        world[line] = value
+    }
+
+    fun optimize () {
+        if (world ==           instructions)
+            world = optimizer (instructions.toMutableList ())
+    }
+
+    fun deoptimize () {
+        world = instructions
+    }
+
+}
+
+class State (private val defaultval : Int? = null, private val observer : (String, Int) -> Unit = { _, _ -> }) {
+
+    private val registers = mutableMapOf<String, Int> ()
+
+    operator fun get (register : String) = if (defaultval == null) registers[register]
+                                           else                    registers.computeIfAbsent (register) { defaultval }
+
+    operator fun set (register : String, value : Int) {
+        registers[register] = value
+
+        observer (register,   value)
+    }
+
+    fun <T> map (f : State.(String, Int) -> T) = registers.map { (k, v) -> f (k, v) }
+
+    fun convertOrLoad (value : String) = value.number { toInt () } ?: load (value)
+
+    fun load (register : String) = this[register]!!
+
+}
+
+class Interpreter (private val code : Code) {
+
+    fun run (state : State) : State {
+        tailrec fun next (pos : Int, state : State) : State {
+            if           (pos >= code.size) return state
+
+            return next (call (pos, state), state)
+        }
+
+        return next (0, state)
+    }
+
+    private fun call (pos : Int, state : State) : Int {
+                      code.optimize ()
+                      code.line = pos
+        val command = code[pos]!!
+
+        return code.line + command.apply (code, state)
+    }
+
+}
+
+interface Command {
+
+    fun apply (code : Code, state : State) : Int
+
+}
